de3bda1e3cdf71e72ebe40e7d9457da0508d673c
JanLJL <jan.laukemann@fau.de>
2020-07-24 14:56:13 +0200

    added sample SVE instruction

---
+++ include_SVE.mk
@@ -0,0 +1,6 @@
+CC 		=  gcc	
+AS 		=  gcc
+CFLAGS 	= 	-O3  -msve-vector-bits=512 -march=armv8.2-a+sve
+LFLAGS 	=  	-shared
+
+KERNELS	+= 	$(patsubst $(SRC_DIR)/%.S, %.so, $(wildcard $(SRC_DIR)/SVE/*.S))
