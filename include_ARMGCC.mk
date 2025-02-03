bbc4be978e77baa066215912e1eb56bc207d0be3
JanLJL <jan.laukemann@fau.de>
2025-02-03 19:08:00 +0100

    fixed bugs in ARM benchmark files

--- include_ARMGCC.mk
+++ include_ARMGCC.mk
@@ -1,8 +1,8 @@
 CC 		= 	gcc
 AS 		= 	gcc
-CFLAGS 	= 	-O3 -march=armv8.5-a
+CFLAGS 	= 	-O3 -march=armv9.2-a+sve
 # -msve-vector-bits=512 -march=armv8.2-a+sve
-ASFLAGS =   -O3 -march=armv8.5-a
+ASFLAGS =   -O3 -march=armv9.2-a+sve
 LFLAGS 	=  	-shared
 
 KERNELS	+= 	$(patsubst $(SRC_DIR)/%.S, %.so, $(wildcard $(SRC_DIR)/BASE-ARM64/*.S))
