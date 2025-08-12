3a9ffd642455fed7a8ff297ae2ce3d8a878c12f8
JanLJL <jan.laukemann@fau.de>
2025-08-12 17:52:43 +0200

    some RISC-V instructions

--- include_GCC.mk
+++ include_GCC.mk
@@ -1,10 +1,14 @@
 CC 		= 	gcc
 AS 		= 	gcc
-CFLAGS 	= 	-O3 -x assembler-with-cpp
-ASFLAGS =   -O3 -x assembler-with-cpp
+#CFLAGS 	= 	-O3 -march=rv64gcv -x assembler-with-cpp
+CFLAGS 	= 	-O3 -march=rv64gcv
+#ASFLAGS =   -O3 -march=rv64gcv -x assembler-with-cpp
+ASFLAGS =   -O3 -march=rv64gcv
 LFLAGS 	= 	-shared
 
-KERNELS	+= 	$(patsubst $(SRC_DIR)/%.S, %.so, $(wildcard $(SRC_DIR)/BASE-X86/*.S))
-KERNELS	+= 	$(patsubst $(SRC_DIR)/%.S, %.so, $(wildcard $(SRC_DIR)/SSE4.2/*.S))
-KERNELS	+= 	$(patsubst $(SRC_DIR)/%.S, %.so, $(wildcard $(SRC_DIR)/AVX2/*.S))
-KERNELS	+= 	$(patsubst $(SRC_DIR)/%.S, %.so, $(wildcard $(SRC_DIR)/AVX512/*.S))
+KERNELS    +=  $(patsubst $(SRC_DIR)/%.S, %.so, $(wildcard $(SRC_DIR)/BASE-RISC-V/*.S))
+KERNELS    +=  $(patsubst $(SRC_DIR)/%.S, %.so, $(wildcard $(SRC_DIR)/RVV/*.S))
+#KERNELS	+= 	$(patsubst $(SRC_DIR)/%.S, %.so, $(wildcard $(SRC_DIR)/BASE-X86/*.S))
+#KERNELS	+= 	$(patsubst $(SRC_DIR)/%.S, %.so, $(wildcard $(SRC_DIR)/SSE4.2/*.S))
+#KERNELS	+= 	$(patsubst $(SRC_DIR)/%.S, %.so, $(wildcard $(SRC_DIR)/AVX2/*.S))
+#KERNELS	+= 	$(patsubst $(SRC_DIR)/%.S, %.so, $(wildcard $(SRC_DIR)/AVX512/*.S))
